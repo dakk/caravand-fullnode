8404c8d6cd2f1c3a5e06525261ab7d9d9188b927
Davide Gessa <gessadavide@gmail.com>
2017-02-03 18:56:16 +0100

    eval

--- src/blockchain/script.mli
+++ src/blockchain/script.mli
@@ -139,7 +139,7 @@ type opcode =
 
 val opcode_to_hex : opcode -> int list
 val opcode_of_hex : bytes -> opcode * bytes
-val eval : opcode list -> bool
+val eval : int Stack.t -> opcode list -> bool
 
 type t = opcode list * int
 
