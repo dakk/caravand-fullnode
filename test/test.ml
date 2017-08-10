f5c9df35557f1836844e7456da56801fee90190b
Davide Gessa <gessadavide@gmail.com>
2017-08-10 15:01:53 +0200

    unit test suite

---
+++ test/test.ml
@@ -0,0 +1,23 @@
+open Bitcoinml;;
+open Blockchain;;
+open Stdint;;
+open OUnit2;;
+open Hex;;
+
+
+let branch_serialize_test rawh octx =
+	let bl = Block.Header.parse (Hex.to_string rawh) in
+	match bl with
+	| None -> assert_equal true false
+	| Some (b) -> 
+    let br = Branch.create b.hash (Int64.of_int 123) b in
+		let brb = Branch.serialize br in
+		let _ = Branch.parse brb in
+    assert_equal true true
+;;
+
+let suite = "letchain" >::: [
+	"branch.serialize_and_parse" >:: branch_serialize_test (`Hex "02000000f6e1cc50df9bfb420162e365fd26d783581367c0a4a7f2683ee60702000000000e65cda8974f3989caeafcaa46ad665ffd07fe558cb63f3f639fee284db83aa4436c6b500045011cec2b25fb");
+];;
+
+let () = run_test_tt_main suite;;
